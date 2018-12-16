405dbad73fc3cd0eceea7d2f6d7b841a46a5398f
NGUYEN TIEN VIET <127phudu@gmail.com>
2018-12-16 22:56:39 +0700

    authentication

--- laravel_multi_authentication/routes/web.php
+++ laravel_multi_authentication/routes/web.php
@@ -23,4 +23,5 @@ Route::prefix('admin')->group(function() {
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
     Route::get('/home', 'AdminController@index')->name('admin.home');
+    Route::get('/dashboard', 'AdminController@index');
 });
